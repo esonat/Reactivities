994f52baa11598329de628a4d2381df3cd9377e4
esonat <enginsonat86@hotmail.com>
2023-01-19 17:01:03 +0300

    End of section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,49 +1,22 @@
-import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Outlet, useLocation } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
 
 function App() {
-  const {activityStore} = useStore();
-
-  useEffect(() => {
-      activityStore.loadActivities();
-  }, [activityStore])
-
-  
-  // function handleCreateOrEditActivity(activity: Activity) {
-  //   setSubmitting(true);
-  //   if (activity.id) {
-  //     agent.Activities.update(activity).then(() => {
-  //       setActivities([...activities.filter(x => x.id !== activity.id), activity])
-  //       setSelectedActivity(activity);
-  //       setEditMode(false);
-  //       setSubmitting(false);
-  //     })
-  //   }else {
-  //     activity.id = uuid();
-  //     agent.Activities.create(activity).then(() => {
-  //       setActivities([...activities, activity])
-  //       setSelectedActivity(activity);
-  //       setEditMode(false);
-  //       setSubmitting(false);
-  //     })
-  //   }
-  // }
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+  const location = useLocation();
 
   return (
     <>
-      <NavBar />
-      <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard />
-      </Container>
-
+      {location.pathname === '/' ? <HomePage /> : (
+        <> 
+          <NavBar />
+          <Container style={{marginTop: '7em'}}>
+            <Outlet /> 
+          </Container>
+        </>
+      )}
     </>
   );
 }
